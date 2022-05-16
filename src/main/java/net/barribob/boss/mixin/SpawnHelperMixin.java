80606b9068d3f9723ed08d8395d72b302643b026
Michael Yoshimura <miyo6032@colorado.edu>
2022-05-16 07:18:06 -0600

    Test adding modded biomes

---
+++ src/main/java/net/barribob/boss/mixin/SpawnHelperMixin.java
@@ -0,0 +1,25 @@
+package net.barribob.boss.mixin;
+
+import net.barribob.boss.block.MobWardBlock;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpawnHelper.class)
+public abstract class SpawnHelperMixin {
+    @Inject(
+            method = "canSpawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/SpawnGroup;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/world/biome/SpawnSettings$SpawnEntry;Lnet/minecraft/util/math/BlockPos$Mutable;D)Z",
+            at = @At("RETURN"),
+            cancellable = true)
+    private static void canSpawn(ServerWorld world, SpawnGroup group, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, SpawnSettings.SpawnEntry spawnEntry, BlockPos.Mutable pos, double squaredDistance, CallbackInfoReturnable<Boolean> cir) {
+        MobWardBlock.Companion.canSpawn(world, pos, cir);
+    }
+}
